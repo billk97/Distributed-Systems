260c123267e5f57965c7f28882c272ad270deffd
billk97 <bill1897@yahoo.gr>
2019-03-09 00:30:30 +0200

    Md5 cleaned up and ready for use

--- src/Md5.java
+++ src/Md5.java
@@ -3,13 +3,11 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 /**this is a small implementation for the Md5 algorithm used to hash the ip&port
  * and the buslines **/
-//TODO make a function to receive string and return a String but hashed
-//Todo clean it up and make it easy and flexible to user for the rest of the project
 //todo make a class compare tha will compare two Hashes remember to use modulo
 public class Md5 {
     private MessageDigest md;
     private String BusLine;
-
+    /**this function returns the hash of a string**/
     public String HASH(String StringToHash)  {
         try {
             md = MessageDigest.getInstance("MD5");
@@ -22,8 +20,23 @@ public class Md5 {
         String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
         System.out.println(myHash);
         return myHash;
-    }
-
+    }//end HASH
+    /**compares two hashes and returns the bigger one **/
+    public String CompairHashes(String Hash1, String Hash2){
+        int result= Hash1.compareTo(Hash2);
+        //when Hash1>Hash2
+        if(result==1)
+        {
+            return Hash1;
+        }
+        //when Hash2>Hash1
+        else if(result==-1)
+        {
+            return Hash2;
+        }//when Hash1=Hash2
+        else return Hash1;
+    }//end compare
+    /**a small implementation/example/test**/
     public static void main(String[] args) {
         Md5 m5 =new Md5();
         m5.HASH("172"+Integer.toString(420));
