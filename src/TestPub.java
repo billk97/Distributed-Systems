dfa45e398b7a761aa851c387a681a93323d69465
billk97 <bill1897@yahoo.gr>
2019-04-05 16:43:40 +0300

    checked if bug has been fixed

--- src/TestPub.java
+++ src/TestPub.java
@@ -6,7 +6,7 @@ public class TestPub {
     public static void main(String[] args) {
         //Publisher p1 = new Publisher(4201,"192.168.1.65",0,20);
         Publisher p1 = new Publisher(4201,"172.16.2.11",0,20);
-        p1.setBrokerIp("172.16.2.11");
+        p1.setBrokerIp("172.16.2.12");
         p1.setBrokerPort(4202);
         p1.startPublisher();
     }
